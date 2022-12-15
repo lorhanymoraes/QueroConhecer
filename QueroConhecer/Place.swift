78e46633adc7b861c0345c0e3b4e3d6aaa8ee89e
lorhanymoraes <107012585+lorhanymoraes@users.noreply.github.com>
2022-12-15 15:36:55 -0300

    project

---
+++ QueroConhecer/Place.swift
@@ -0,0 +1,55 @@
+//
+//  Place.swift
+//  QueroConhecer
+//
+//  Created by Lorhany Moraes on 06/06/22.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+
+struct Place: Codable {
+    let name: String
+    let latitude: CLLocationDegrees
+    let longitude: CLLocationDegrees
+    let address: String
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static func getFormattedAddress(with placemark: CLPlacemark) -> String {
+        var address = ""
+        if let street = placemark.thoroughfare {
+            address += street //Rua
+        }
+        if let number = placemark.subThoroughfare {
+            address += "\(number)" //Número
+        }
+        if let subLocality = placemark.subLocality {
+            address += ", \(subLocality)" //Bairro
+        }
+        if let city = placemark.locality {
+            address += "\n\(city)" //Cidade
+        }
+        if let state = placemark.administrativeArea {
+            address += " - \(state)" //Estado
+        }
+        if let postalCode = placemark.postalCode {
+            address += "\nCEP: \(postalCode)" //Cep
+        }
+        if let country = placemark.country {
+            address += "\n\(country)" //País
+        }
+        return address
+    }
+        
+}
+
+extension Place: Equatable {
+    static func ==(lhs: Place, rhs: Place) -> Bool {
+        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+}
